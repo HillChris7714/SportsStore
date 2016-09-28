12bd4b9f2f2ac1c7ab5bc157bdfdef2435063981
CHill <CHill@3YBS382>
2016-09-28 12:47:01 -0700

    Add project files.

---
+++ SportsStore.Domain/Concrete/EmailOrderProcessor.cs
@@ -0,0 +1,16 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EmailOrderProcessor
+    {
+    }
+}
